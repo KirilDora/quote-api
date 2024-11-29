c14a52ac2270cd04bcd511e6d06edb7a092261f6
KirilDora <kirildora02@gmail.com>
2024-11-29 14:15:26 +0200

    Added base functionality

--- server.js
+++ server.js
@@ -9,3 +9,39 @@ const PORT = process.env.PORT || 4001;
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`App listening on Port: ${PORT}`));
+
+quotesRouter = express.Router();
+app.use('/api/quotes', quotesRouter);
+
+quotesRouter.get('/random', (req, res) => {
+  const quote = getRandomElement(quotes);
+  if(quote) {
+    res.send(quote);
+  } else {
+    res.status(404).send();
+  }
+});
+
+quotesRouter.get('/', (req, res) => {
+  const {person} = req.query;
+  if(person) {
+    const byPerson = quotes.filter((quote) => quote.person === person );
+    if(byPerson) {
+      res.send({quotes: byPerson});
+    } else {
+      res.send([]);
+    }
+  } else { 
+    res.send(quotes);
+  }
+});
+
+quotesRouter.post('/', (req, res) => {
+  const {quote, person} = req.query;
+  if(quote && person) {
+    quotes.push({quote, person});
+    res.send(quotes[quotes.length-1]);
+  } else {
+    res.status(400);
+  }
+});
